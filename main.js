d37222331611712711c28e0a6a01e7ffab8fb06f
Eno <urboieno12@gmail.com>
2022-09-08 07:22:01 +0900

    files set up

---
+++ main.js
@@ -0,0 +1,11 @@
+/* target the button and create an api*/ 
+
+/* try and use asycn and await */ 
+
+/* discuss what we want to do / what we want to display onto our DOM */ 
+
+/* How should the work be divided ?  */ 
+
+/* optimize code */ 
+
+/* Push? add carousel */ 
