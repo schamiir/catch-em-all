b27b24223b7cfcf18d799e68a31c2239beb28967
Eno <urboieno12@gmail.com>
2022-09-14 08:22:04 +0900

    solve the issue

--- main.js
+++ main.js
@@ -8,28 +8,25 @@
 
 document.getElementById("button").addEventListener("click", getfetch)
 // Capitalize Function
-function capitalizeName(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1)
+function capitalizeName() {
+   // create a function that capitalizes a string - we want to make our pokemon names and other contents capitalized! 
 }
+
+// at - s
+
 
 // array = data.abilities
 
-function abilityMaker(skillsData){
-    const abilityArr = skillsData.abilities // array
-    const ul = document.getElementById("abilities")
-    ul.innerText = ""
-    for (let i = 0; i < abilityArr.length; i++) {
-        let li = document.createElement('li')
-        ul.appendChild(li).innerText = `Ability ${i+1}: ${abilityArr[i].ability.name}`
-    }
+function abilityMaker(){
+    // create a function that creates a list of abilities. The length of the list will be dependent on the pokemon! 
+    // and display it on the dom -
 }
 
 // Ability i:  limber abilityArr[i].ability.name
 //   document.getElementById('type').innerText = `Type: ${capitalizeName(type)}`
 
 // limber => Ability 1: Limber
-// innterText = limber 
-// innerText = Ability 1: Lim
+// Ability 1: Lim
 
 function getfetch(){
     const userInput = document.querySelector('input').value.toLowerCase()
@@ -39,11 +36,7 @@ function getfetch(){
     .then(response => response.json())
     .then(data => {
         console.log(data)
-        //setting const variable equal to grabbed API data
         const name = data.name
-        // slice(startIndex, endIndex) // slice(0,3) Dit does not include the last  
-        // slice(1) itto
-        // D + itto
         const type = data.types[0].type.name  
         
         const image = data.sprites.other["official-artwork"]["front_default"]    
@@ -61,12 +54,6 @@ function getfetch(){
         document.querySelector('img').src = image
         
         abilityMaker(data)
-        /*
-        Ability 1 : Limber 
-        Ability 2: Imposter 
-        */
-
-        //const abilities = data.abilities[0].ability.name
     })
 }
 
